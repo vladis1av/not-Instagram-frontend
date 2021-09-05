ec93c09913f48b0af783fe041bd196df782bd336
Vladis1av <vladfrolovcsgo@gmail.com>
2021-09-05 23:56:04 +0300

    Изменил API_URL

--- src/core/axios.js
+++ src/core/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_URL = `http://localhost:8888`;
+export const API_URL = `https://not-instagram-backend.herokuapp.com`;
 
 const $api = axios.create({
   withCredentials: true,
